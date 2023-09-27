a84b9b29c59c78d80eb24ac4315a19c7517e4e44
Aphatheology <mustapha.abdulkareem@saritay.com>
2023-09-27 09:41:53 +0100

    Week 1 assignment

---
+++ src/Motor.java
@@ -0,0 +1,15 @@
+public class Motor {
+    private String brand;
+    private String model;
+    private String color;
+    private int yearManufactured;
+    private boolean isElectric;
+
+    public void brake() {
+        System.out.println("This motor will stop in 5 seconds.");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Mustapha Abdulkareem's Assignment");
+    }
+}
