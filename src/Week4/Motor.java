a8652518de3f292e01fbcf92bd2df535e04bd443
Aphatheology <mustapha.abdulkareem@saritay.com>
2023-10-23 13:45:14 +0100

    update exception

--- src/Week4/Motor.java
+++ src/Week4/Motor.java
@@ -1,12 +1,10 @@
 package Week4;
 
 public class Motor {
-    public void drive(int speed) throws DriverOverSpeedException {
-
+    public void drive(int speed) {
         try {
             if (speed <= 500) System.out.println("Enjoy your journey");
-            else if (speed <= 550) throw new DriverOverSpeedException("You are Over-speeding, be careful!");
-            else throw new DriverOverSpeedException("You are extremely Over-speeding, death ahead!!!");
+            else throw new DriverOverSpeedException("You are Over-speeding, death ahead!!!");
         } catch (DriverOverSpeedException error) {
             System.out.println(error.getMessage());
         }
@@ -16,10 +14,8 @@ public class Motor {
     public static void main(String[] args) throws DriverOverSpeedException {
         Motor sensibleDriver = new Motor();
         Motor recklessDriver = new Motor();
+
         recklessDriver.drive(600);
         sensibleDriver.drive(350);
-        recklessDriver.drive(540);
-
-
     }
 }
