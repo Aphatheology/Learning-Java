5b808481ce2f93adf3b060c3610bb2354af1b805
Aphatheology <mustapha.abdulkareem@saritay.com>
2023-10-28 10:44:18 +0100

    Week 6 Assignment

---
+++ src/Week5/Aphatheology-Servlet/WEB-INF/classes/FormSubmissionServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FormSubmissionServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String message = request.getParameter("message");
+
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Message: " + message);
+
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.write("<h1>Form submitted successfully</h1>");
+        pw.close();
+    }
+}
--- src/Week5/README.md
+++ src/Week5/README.md
@@ -1,7 +1,13 @@
-# Week 5
+# Week 5 and 6
 
-## Assignment 
+## Assignment for Week 5
 Create a project and a servlet by following the structure of Java web server. The servlet should be map to url(e.g your name), then the response of the servlet should be full name in the servlet. Compile and deploy the servlet
+
+## Assignment for Week 6
+- Create a project following the server structure for Java Web. The web page should return anything of your choice.
+- Create a Servlet and define a route. The first route should return an HTML form requesting for username and password.
+- When the form is submitted, the body data of the form should logged to the console.
+
 
 ## Useful Command Lines
 - To compile the Java file: `javac -cp "C:\Program Files\Apache Software Foundation\Tomcat 9.0\lib\servlet-api.jar" AphatheologyServlet.java`
